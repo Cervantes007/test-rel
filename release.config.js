8306eb58b0c380706db2319e2d0e2ad710a0194f
gsi-alejandro <alejandro@gsoftinnovation.com>
2022-02-01 07:06:10 -0500

    feat(release): adding semantic-release

---
+++ release.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  branches: ['main', { name: 'beta', prerelease: true }],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md'],
+      },
+    ],
+    '@semantic-release/github',
+  ],
+};
